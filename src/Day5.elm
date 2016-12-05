bac22d7b30199b6e36c06a98ca94f0438dc22387
jjelfs <jjelfs@travelrepublic.co.uk>
2016-12-05 19:29:10 +0000

    day 5 part 2 - silly mistake

--- src/Day5.elm
+++ src/Day5.elm
@@ -22,47 +22,43 @@ crack n pw =
                         _ -> crack (n + 1) p
             _ -> crack (n + 1) pw
 
-validPos s =
-    case s of
-        Ok n ->
-            case n >= 0 && n <= 7 of
-                True -> Just n
-                False -> Nothing
-        Err _ -> Nothing
-
-validCode slots n c =
+slotEmpty slots n =
     case get n slots of
         Nothing -> False
         Just s -> s == "_"
+
+validPos slots md5 =
+    case String.slice 5 6 md5 |> toInt of
+        Ok n ->
+            if n >= 0 && n <= 7 && (slotEmpty slots n) then
+                Just n
+            else
+                Nothing
+        Err _ -> Nothing
 
 slotsFilled slots =
     filter (\s -> s == "_") slots
         |> isEmpty
 
---call this with 0 "" (repeat 8 -1) to get the answer
+--call this with 0 "" (repeat 8 "_") to get the answer
 crack2 n slots =
     let
         md5 = hex (input ++ (toString n))
-        front = String.slice 0 5 md5
-        pos = String.slice 5 6 md5 |> toInt
-        code = String.slice 6 7 md5
     in
-        case front == "00000" of
-            False -> crack2 (n + 1) slots
-            True ->
-                case validPos pos of
-                    Nothing -> crack2 (n + 1) slots
-                    Just i ->
-                        case validCode slots i code of
-                            False -> crack2 (n + 1) slots
-                            True ->
-                                let
-                                    updated = set i code slots
-                                in
-                                    case slotsFilled updated of
-                                        True ->
-                                            updated
-                                                |> toList
-                                                |> (List.foldl (++) "")
-                                        _ -> crack2 (n + 1) updated
+        if String.slice 0 5 md5 == "00000" then
+            case validPos slots md5 of
+                Nothing -> crack2 (n+1) slots
+                Just i ->
+                    let
+                        updated = set i (String.slice 6 7 md5) slots
+                    in
+                        if slotsFilled updated then
+                            updated
+                                |> toList
+                                |> (List.foldr (++) "")
+                        else
+                            crack2 (n+1) updated
+        else
+            crack2 (n+1) slots
+
 
