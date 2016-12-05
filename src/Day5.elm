7f7039b49f921cae746e4f49e40520c6c1382cfe
MERIDIAN\jjelfs <jjelfs@travelrepublic.co.uk>
2016-12-05 16:56:27 +0000

    day5 part 2 - don't think this is right

--- src/Day5.elm
+++ src/Day5.elm
@@ -1,6 +1,8 @@
 module Day5 exposing (..)
 
 import MD5 exposing (..)
+import Array exposing (..)
+import String exposing (toInt, length, fromList)
 
 input = "reyedfim"
 
@@ -18,3 +20,48 @@ crack n pw =
                         8 -> p
                         _ -> crack (n + 1) p
             _ -> crack (n + 1) pw
+
+validPos s =
+    case s of
+        Ok n ->
+            case (n >= 0 && n <= 7, n) of
+                True -> Just n
+                False -> Nothing
+        Err _ -> Nothing
+
+validCode slots n c =
+    case get n slots of
+        Nothing -> False
+        Just s -> s == "_" || s /= c
+
+slotsFilled slots =
+    filter (\s -> s == "_") slots
+        |> isEmpty
+
+--call this with 0 "" (repeat 8 -1) to get the answer
+crack2 n slots =
+    let
+        md5 = hex (input ++ (toString n))
+        front = String.slice 0 5 md5
+        pos = String.slice 5 6 md5 |> toInt
+        code = String.slice 6 7 md5
+    in
+        case front == "00000" of
+            False -> crack2 (n + 1) slots
+            True ->
+                case validPos pos of
+                    Nothing -> crack2 (n + 1) slots
+                    Just n ->
+                        case validCode slots n code of
+                            False -> crack2 (n + 1) slots
+                            True ->
+                                let
+                                    updateSlots = set n code slots
+                                in
+                                    case slotsFilled updateSlots of
+                                        True ->
+                                            updateSlots
+                                                |> toList
+                                                |> (List.foldl (++) "")
+                                        _ -> crack2 (n + 1) updateSlots
+
