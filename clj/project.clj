4726c00925f0c6be2658ebb45987be339e4a506b
MERIDIAN\jjelfs <jjelfs@travelrepublic.co.uk>
2016-12-16 09:28:23 +0000

    added some profiling - but can I be bothered

--- clj/project.clj
+++ clj/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.namespace "0.2.11"]
+                 [com.taoensso/tufte "1.1.0"]
                  [org.clojure/math.combinatorics "0.1.3"]])
