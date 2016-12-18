921c8e0d07ec0b579ee40db5d538468d436cdbfb
jjelfs <jjelfs@travelrepublic.co.uk>
2016-12-18 09:56:19 +0000

    bit more day 13

--- src/Day13.elm
+++ src/Day13.elm
@@ -1,18 +1,54 @@
 module Day13 exposing (..)
 
+import Bitwise exposing (and, shiftRightBy)
+import Set
+
 input = 1350
 
-{--
-from current position find all possible next positions
-for each one, recurse. Breadth first search until you find (31,39)
-This is almost exactly the same as day 11
---}
+positionValid visited (x, y) =
+    positive (x, y)
+        && (not (Set.member (x, y) visited))
+        && available (x, y)
+
+positive (x, y) =
+    x >= 0 && y >= 0
+
+available (x, y) =
+    (((x * x + 3 * x + 2 * x * y + y + y * y) + input )
+        |> setBits) % 2 == 0
+
+-- http://www.playingwithpointers.com/swar.html
+setBits i =
+    let
+        a =
+            i - ( i
+                |> shiftRightBy 1
+                |> and 0x55555555 )
+        b =
+            ( and a 0x33333333 ) + ( a |> shiftRightBy 2 |> and 0x33333333 )
+    in
+        ( ( b + ( shiftRightBy 4 b ) ) |> and 0x0F0F0F0F ) * 0x01010101 |> shiftRightBy 24
+
+nearbyCells (x, y) =
+    let
+        inc n = n + 1
+        dec n = n - 1
+        id n = n
+    in
+    [ ( dec, dec )
+    , ( dec, id )
+    , ( dec, inc )
+    , ( id, dec )
+    , ( id, inc )
+    , ( inc, dec )
+    , ( inc, id )
+    , ( inc, inc )
+    ] |> List.map
+        (\(xfn, yfn) -> (xfn x, yfn y))
 
 getPossiblePositions visited position =
-    {-- get all the surrounding positions
-     filter out anything that has a negative element
-     filter out anything that is a wall --}
-    [position]
+     (nearbyCells position)
+        |> List.filter (positionValid visited)
 
 evaluationPositions depth positions visited =
     let
@@ -35,8 +71,6 @@ evaluationPositions depth positions visited =
                 depth
             False ->
                 evaluationPositions (depth + 1) n v
-
---one more thing to try which is to store visited pairs in a set
 
 solution () =
     evaluationPositions 0 [(1,1)] Set.empty
