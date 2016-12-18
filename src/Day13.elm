0708cee8afd9d73ec247b31557cef37a83a7ac8f
jjelfs <jjelfs@travelrepublic.co.uk>
2016-12-18 21:10:44 +0000

    day 13 done"

--- src/Day13.elm
+++ src/Day13.elm
@@ -15,10 +15,11 @@ positive (x, y) =
 
 available (x, y) =
     (((x * x + 3 * x + 2 * x * y + y + y * y) + input )
-        |> setBits) % 2 == 0
+        |> numberOfBitsSet) % 2 == 0
 
--- http://www.playingwithpointers.com/swar.html
-setBits i =
+--http://www.playingwithpointers.com/swar.html
+--I have no clue how this works
+numberOfBitsSet i =
     let
         a =
             i - ( i
@@ -30,47 +31,27 @@ setBits i =
         ( ( b + ( shiftRightBy 4 b ) ) |> and 0x0F0F0F0F ) * 0x01010101 |> shiftRightBy 24
 
 nearbyCells (x, y) =
-    let
-        inc n = n + 1
-        dec n = n - 1
-        id n = n
-    in
-    [ ( dec, dec )
-    , ( dec, id )
-    , ( dec, inc )
-    , ( id, dec )
-    , ( id, inc )
-    , ( inc, dec )
-    , ( inc, id )
-    , ( inc, inc )
-    ] |> List.map
-        (\(xfn, yfn) -> (xfn x, yfn y))
+    [(x, y-1), (x, y+1), (x-1, y), (x+1, y)]
 
 getPossiblePositions visited position =
      (nearbyCells position)
         |> List.filter (positionValid visited)
 
-evaluationPositions depth positions visited =
+evaluatePositions depth positions visited =
     let
-        (f, v, n) =
-            List.foldl (\p (foundSolution, visited, nextLevel) ->
-                if foundSolution then
-                    (foundSolution, visited, nextLevel)
-                else
-                    case p == (31, 39) of
-                        True -> (True, visited, nextLevel)
-                        False ->
-                            let
-                                v = Set.insert p visited
-                            in
-                                (foundSolution, v, List.append nextLevel (getPossiblePositions v p))
-            ) (False, visited, []) positions
+        (v, n) =
+            List.foldl (\p (visited, nextLevel) ->
+                let
+                    v = Set.insert p visited
+                in
+                    (v, List.append nextLevel (getPossiblePositions v p))
+            ) (visited, []) positions
     in
-        case f of
+        case depth == 50 of
             True ->
-                depth
+                Set.size v
             False ->
-                evaluationPositions (depth + 1) n v
+                evaluatePositions (depth + 1) n v
 
 solution () =
-    evaluationPositions 0 [(1,1)] Set.empty
+    evaluatePositions 0 [(1,1)] Set.empty
