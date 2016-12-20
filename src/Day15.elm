05eb6fe4902529efd863cd59a68e2f30abe41b40
jjelfs <jjelfs@travelrepublic.co.uk>
2016-12-20 21:56:01 +0000

    day 15 1 & 2

--- src/Day15.elm
+++ src/Day15.elm
@@ -2,62 +2,45 @@ module Day15 exposing (..)
 
 import Array exposing (Array)
 
-type alias Disc =
-    { position: Int
-    , slots: Int }
-
-type alias State =
-    { time: Int
-    , discs: Array Disc }
-
-numberOfDiscs = 2
-
---initialState =
---    State
---        0
---        Array.fromList
---            [ Disc 15 17
---            , Disc 2 3
---            , Disc 4 19
---            , Disc 2 13
---            , Disc 2 7
---            , Disc 0 5 ]
-
 initialState =
-    State
-        0
-        (Array.fromList
-            [ Disc 4 5
-            , Disc 1 2 ])
+    Array.fromList
+        [ (15, 17)
+        , (2, 3)
+        , (4, 19)
+        , (2, 13)
+        , (2, 7)
+        , (0, 5)
+        , (0, 11)]
 
---this is not right, need to do a % thing
-shiftPosition seconds disc =
-    { disc | position = disc.position + seconds - disc.slots }
+shiftPosition seconds (position, slots) =
+    ((position + seconds) % slots, slots)
 
 translate state seconds =
-    { state | time = seconds
-    , discs = (Array.map (shiftPosition seconds) state.discs) }
-
-
-{--
-    run the simulation with incrementing start times
-    for each start time we then increment the time. With each
-    tick we translate the state by + 1 and make sure that the disc
-    at index t is at position 0. If not abandon, if so, next tick
---}
+    (Array.map (shiftPosition seconds) state)
 
 runSimulation startTime =
     let
-        start = translate initialState startTime
+        (success, _) =
+            List.foldl
+                (\i (o, s) ->
+                    if o then
+                        let
+                            nextState = translate s 1
+                            open =
+                                case Array.get i nextState of
+                                    Just (position, _) -> position == 0
+                                    Nothing -> False
+                        in
+                            (o && open, nextState)
+                    else
+                        (o, s)
+                ) (True, translate initialState startTime)
+                (List.range 0 ((Array.length initialState)-1))
     in
-        List.foldl
-            (\i (o, s) ->
-                let
-                    nextState = translate s 1
-                    open =
-                        case Array.get i s.discs of
-                            Just d -> d.position == 0
-                            Nothing -> False
-                in
-                    (o && open, nextState)
-            ) (True, start) (List.range 0 (numberOfDiscs-1))
+        if success then
+            startTime
+        else
+            runSimulation (startTime + 1)
+
+solution () =
+    runSimulation 0
