f894a7659b3156caa35170d7acd0a4628a72a255
jjelfs <jjelfs@travelrepublic.co.uk>
2017-03-05 11:32:19 +0000

    added some type signatures

--- ps/src/Day1.purs
+++ ps/src/Day1.purs
@@ -3,10 +3,11 @@ module Day1 where
 import Prelude
 import Data.List (List, foldl)
 import Data.Tuple
-import Data.String (Pattern(..), split, uncons, take, drop)
+import Data.String (Pattern(..), split, take, drop)
 import Data.Int (fromString)
 import Data.Maybe (fromMaybe)
 
+input :: String
 input =
     "R4, R3, R5, L3, L5, R2, L2, R5, L2, R5, R5, R5, R1, R3, L2, L2, L1, R5, L3, R1, L2, R1, L3, L5, L1, R3, L4, R2, R4, L3, L1, R4, L4, R3, L5, L3, R188, R4, L1, R48, L5, R4, R71, R3, L2, R188, L3, R2, L3, R3, L5, L1, R1, L2, L4, L2, R5, L3, R3, R3, R4, L3, L4, R5, L4, L4, R3, R4, L4, R1, L3, L1, L1, R4, R1, L4, R1, L1, L3, R2, L2, R2, L1, R5, R3, R4, L5, R2, R5, L5, R1, R2, L1, L3, R3, R1, R3, L4, R4, L4, L1, R1, L2, L2, L4, R1, L3, R4, L2, R3, L1, L5, R4, R5, R2, R5, R1, R5, R1, R3, L3, L2, L2, L5, R2, L2, R5, R5, L2, R3, L5, R5, L2, R4, R2, L1, R3, L5, R3, R2, R5, L1, R3, L2, R2, R1"
 
@@ -26,14 +27,17 @@ type State =
     , pos :: Coord
     }
 
+initialState :: State
 initialState =
     { facing : North
     , pos : { x : 0, y : 0 }
     }
 
+extractNumSteps :: String -> Int
 extractNumSteps =
     drop 1 >>> fromString >>> fromMaybe 0
 
+toTuple :: String -> Tuple String Int
 toTuple instr =
   let
       d = take 1 instr
@@ -72,6 +76,7 @@ move {facing, pos} (Tuple "L" n) =
         { facing : d, pos : changePos d n pos }
 move state _ = state
 
+parseInstructions :: String -> Array (Tuple String Int)
 parseInstructions =
     split (Pattern ", ") 
         >>> map toTuple 
@@ -79,8 +84,9 @@ parseInstructions =
 distance {pos : {x,y}} =
     x + y
 
+processInstructions :: String -> Int
 processInstructions inp =
-    distance $ foldl move initialState $ parseInstructions inp
+    distance $ foldl move initialState $ parseInstructions inp 
 
 showState {facing, pos : { x, y }} =
     (case facing of
